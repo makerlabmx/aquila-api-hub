5afcaf7926c3ea989cc89a620336b8e2f17f963b
Rodrigo Mendez <rmendez@makerlab.mx>
2014-12-16 13:25:55 -0600

    changes to launcher.js for windows compat

--- launcher.js
+++ launcher.js
@@ -6,6 +6,10 @@ var config = require("shelljs").config;
 var path = require("path");
 
 var home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+var killall = (process.platform == 'win32') ? 'taskkill /IM' : 'killall';
+
+echo("Aquila Server\n");
+echo("IMPORTANT: Make shure that the bridge is connected.\n");
 
 // cd to where this file lives
 cd(__dirname);
@@ -20,12 +24,15 @@ rm(path.join(home, ".aquila-server/data/mongodb.log"));
 config.silent = false;
 
 // Start MongoDB
+// for multi platform compatibility:
+var dbpath = path.join(home, ".aquila-server/data/db");
+var logpath = path.join(home, ".aquila-server/data/mongodb.log");
 echo("Starting Database...");
-exec("mongod --dbpath ~/.aquila-server/data/db --logpath ~/.aquila-server/data/mongodb.log", { async: true });
+exec("mongod --dbpath " + dbpath + " --logpath " + logpath, { async: true });
 
 // Start aquila-server
 echo("Starting Aquila Server...");
 exec("node aquila-server.js", function(code, output)
   {
-    exec("killall mongod");
+    exec(killall + " mongod");
   });
