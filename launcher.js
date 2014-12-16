c1ae1b130ad2ea8d23c6c7d117acb5ef49079345
Rodrigo Mendez <rmendez@makerlab.mx>
2014-12-16 13:44:45 -0600

    more changes to launcher.js for windows

--- launcher.js
+++ launcher.js
@@ -6,7 +6,7 @@ var config = require("shelljs").config;
 var path = require("path");
 
 var home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-var killall = (process.platform == 'win32') ? 'taskkill /IM' : 'killall';
+var killall_mongod = (process.platform == 'win32') ? 'taskkill /F mongod.exe' : 'killall mongod';
 
 echo("Aquila Server\n");
 echo("IMPORTANT: Make shure that the bridge is connected.\n");
@@ -25,14 +25,14 @@ config.silent = false;
 
 // Start MongoDB
 // for multi platform compatibility:
-var dbpath = path.join(home, ".aquila-server/data/db");
-var logpath = path.join(home, ".aquila-server/data/mongodb.log");
+var dbpath = '"' + path.join(home, ".aquila-server/data/db") + '"';
+var logpath = '"' + path.join(home, ".aquila-server/data/mongodb.log") + '"';
 echo("Starting Database...");
-exec("mongod --dbpath " + dbpath + " --logpath " + logpath, { async: true });
+exec("mongod --journal --dbpath " + dbpath + " --logpath " + logpath, { async: true });
 
 // Start aquila-server
 echo("Starting Aquila Server...");
 exec("node aquila-server.js", function(code, output)
   {
-    exec(killall + " mongod");
+    exec(killall_mongod);
   });
