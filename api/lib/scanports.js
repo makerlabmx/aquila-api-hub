c615a14ddd778575fe87ced103cb432cc7b5e7cc
Rodrigo Mendez <rmendez@makerlab.mx>
2015-01-01 17:21:39 -0600

    new synchronous scanports inplementations, should be more reliable

--- api/lib/scanports.js
+++ api/lib/scanports.js
@@ -4,6 +4,8 @@ var async = require("async");
 var buffertools = require("buffertools");
 var os = require("os");
 
+var TIMEOUT = 1000;
+
 var scanPorts = function(baudrate, callback)
 {
 	console.log("Searching for bridge...");
@@ -11,83 +13,71 @@ var scanPorts = function(baudrate, callback)
 	var expectedMessage = new Buffer([0xAA, 0x55, 0xAA, 0x55, 0x04]);
 	var ping = new Buffer([0xAA, 0x55, 0xAA, 0x55, 0x07]);
 
-	Serial.list(function (err, ports) 
+	Serial.list(function (err, ports)
 	{
-		ports.forEach(function(port) 
-		{
-			// console.log(port.comName);
-			try
+
+		var foundPort = "";
+
+		async.forEachSeries(ports, function(port, cb)
 			{
 				// Avoid Bluetooth tty ports as workaround for OSX kernel panics:
-				if(port.comName.indexOf("Bluetooth") !== -1) return;
+				if(port.comName.indexOf("Bluetooth") !== -1) return cb();
+				
+				console.log("  *Trying:", port.comName);
 
-				var p = new SerialPort(port.comName, 
-				{
-					baudrate: baudrate
-				}, true, function(err)
-				{
-					if(err) return console.log(err);
-					var timeout = setTimeout(function()
-					 	{
-					 		try
-					 		{
-					 			p.close();
-					 		}
-					 		catch(err){}
-					 		
-						}, 2000);
-					p.on("data", function(data) 
-						{
-							// console.log(data);
-							// only compare first 5 bytes, same lenght as expectedMessage
-							if(buffertools.compare(data.slice(0, 5), expectedMessage) === 0)
-							{
-								clearTimeout(timeout);
-								console.log("Found bridge in ", port.comName);
-								if(callback)
-								{
-									p.flush(function(err) 
-										{
-							            	p.drain(function(err)
-							            	{
-							                	// Windows needs this, otherwise access to COM is denied
-							                	if(os.platform() === "win32" || os.platform() === "win64")
-							                	{
-							                		setTimeout(function()
-								                	{
-									                	p.close(function(err)
-									                	{
-									                		callback(p.path);
-									                	});
-								                	}, 10);
-							                	}
-							                	else
-							                	{
-							                		p.close(function(err)
-								                	{
-								                		callback(p.path);
-								                	});
-							                	}
-							            		
-							            	});
-							        });
-								}
-							}
-						});
-					try
+				var p = new SerialPort(port.comName,
 					{
-						// Wait if already sending then send
-						p.drain(function()
-						{
-							p.write(ping);
-						});
-					}
-					catch(err){}
-				});
-			}
-			catch(err){}
-			
-  		});
+						baudrate: baudrate
+					}, true, function(err)
+					{
+						if(err) { /*console.log(err);*/ return cb(); }
+
+						var timeout = setTimeout(function()
+							{
+								p.drain(function(err)
+									{
+										if(err) return cb(err);
+										p.close(function(){ cb(); });
+									});
+							}, TIMEOUT);
+
+						p.on("data", function(data)
+							{
+								// only compare first 5 bytes, same lenght as expectedMessage
+								if(buffertools.compare(data.slice(0, 5), expectedMessage) === 0)
+								{
+									clearTimeout(timeout);
+									console.log("Found bridge in ", port.comName);
+
+									p.flush(function(err)
+									{
+										p.drain(function(err)
+										{
+											p.close(function(err)
+											{
+												foundPort = p.path;
+												cb("found");
+											});
+										});
+									});
+								}
+							});
+
+							// Wait if already sending then send ping
+							p.drain(function()
+								{
+									p.write(ping);
+								});
+
+					});
+
+			}, function(err)
+			{
+				if(err === "found") return callback(foundPort);
+				else if(err) { console.log("Error searching for Bridge:", err); process.exit(); }
+				else { console.log("Couldn't find any Bridge."); process.exit(); }
+			});
+
 	});
 };
 
--- config/bridge.js
+++ config/bridge.js
@@ -7,7 +7,7 @@ module.exports = {
 	// Bridge connection parameters:
 	baudrate: 57600,
 	// Serial port, looks like "/dev/tty.usbserial-DA00RNZ1", null = auto
-	port: "/dev/tty.usbserial-DA00RNZ1",
+	port: null,
 	// Fake mode
 	fake: false,
 
