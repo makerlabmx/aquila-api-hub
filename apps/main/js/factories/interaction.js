3bb9fb7166c94a5fa9707bfb076ada58fc432266
Rodrigo Mendez <rmendez@makerlab.mx>
2015-01-22 20:02:00 -0600

    Keep trying to fix badcfg...

--- apps/main/js/factories/interaction.js
+++ apps/main/js/factories/interaction.js
@@ -4,7 +4,7 @@
 
   app.factory('Interaction', ['$resource', '$rootScope', function($resource, $rootScope){
       return $resource($rootScope.server + "api/interactions/:id", {id: "@id"}, {
-          get: { method: 'GET', isArray: true},
+          get: { method: 'GET', isArray: false},
           create: { method: 'POST', isArray:true},
           delete: { method: 'DELETE', isArray:true},
           all: {method:'GET', isArray:true}
