49a7e89e8e84a65013cb5e318cc0dc9054d79418
Odroid <odroid@odroid.com>
2015-03-12 12:53:40 -0600

    Finished restoring database code

--- launcher.js
+++ launcher.js
@@ -54,37 +54,46 @@ var startAquila = function()
       });
   };
 
-var checkConnectionWithDB = function(pingCode)
-{
-  pingCode = exec("nc -z localhost 27017").code;
-  if (pingCode == 0)
+var checkConnectionWithDB = function(callback)
   {
-    clearInterval(intervalID);
+    var pingCode = exec("nc -z localhost 27017").code; //Checks if mongodb is accepting connections
+    if (pingCode == 0)
+    {
+      clearInterval(intervalID);
+      callback();
+    }
+  };
+
+var restoreDatabase = function()
+  {
     console.log("Restoring database");
     exec("mongorestore --drop " + path.join(home, ".aquila-server/backup/aquila-server"));
     console.log("Database restored");
     startAquila();
-  }
+  };
+
+var connectMongoose = function()
+{
+  mongoose.connect(configDB.url, function(err, res)
+  {
+    if(err)
+    {
+      console.log("ERROR connecting to database, will try to restore database.");
+      exec("rm -rf " + path.join(home, ".aquila-server/data/*"), function()
+        {
+          // Create aquila-server config dir if not exists
+          mkdir("-p", path.join(home, ".aquila-server/data/db"));
+          console.log("Created new database folder");
+          exec("mongod --journal --dbpath " + dbpath + " --logpath " + logpath, { async: true });
+          intervalID = setInterval(checkConnectionWithDB, 500, restoreDatabase);
+        });
+    } else 
+    {
+      startAquila();
+    }
+
+  });
 };
 
-mongoose.connect(configDB.url, function(err, res)
-{
-  if(err)
-  {
-    console.log("ERROR connecting to database, will try to restore database.");
-    exec("rm -rf " + path.join(home, ".aquila-server/data/*"), function()
-      {
-        // Create aquila-server config dir if not exists
-        mkdir("-p", path.join(home, ".aquila-server/data/db"));
-        console.log("Created new database folder");
-        exec("mongod --journal --dbpath " + dbpath + " --logpath " + logpath, { async: true });
-        var pingCode = 1;
-        intervalID = setInterval(checkConnectionWithDB, 500, pingCode, intervalID);
-      });
-  } else 
-  {
-    startAquila();
-  }
-
-});
+connectMongoose();
 
