4b98be2f8c0665d5f40655df69742f2ed78cc049
Rodrigo Mendez <rmendez@makerlab.mx>
2015-03-20 11:10:39 -0600

    changed default config for deviceManager

--- config/deviceManager.js
+++ config/deviceManager.js
@@ -5,12 +5,12 @@ module.exports = {
 	// check online devices every 'refreshInterval' milliseconds
 	autoCheckAlive: true,
 	refreshInterval: 30000,
-	activeRefreshInterval: 2000,
+	activeRefreshInterval: 5000,
 	// Ping for Refresh known but inactive devices, or only active ones:
-	refreshInactive: true,
+	refreshInactive: false,
 	// Max number of retries after a device is marked as inactive:
 	maxRetriesInactive: 1,
-	maxRetriesFetch: 3,
+	maxRetriesFetch: 1,
 	//Advanced options, modify only if you know what you are doing:
 	timeout: 200,
 	longTimeout: 1000,
