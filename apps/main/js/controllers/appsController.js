a5767512f2ca9b35344d8b2da4992cb9a88ac3ea
Gilberto Leon <ripflame@gmail.com>
2015-06-05 10:30:16 -0500

    Removing functions

--- apps/main/js/controllers/appsController.js
+++ apps/main/js/controllers/appsController.js
@@ -4,29 +4,26 @@
 
   app.controller('AppsController', function($scope) {
 
-  	$scope.error = false;
-  	$scope.modal_title = "";
+  	$scope.error        = false;
+  	$scope.modal_title  = "";
   	$scope.save_element = false;
   	$scope.edit_element = false;
 
     $scope.newToken = function() {
-      $scope.error = false;
+      $scope.error        = false;
       $scope.save_element = true;
       $scope.edit_element = false;
-      $scope.modal_title = "Create new token";
+      $scope.modal_title  = "Create new token";
       $('#modal-token').modal('show');
     };
 
     $scope.editToken = function(name) {
-      $scope.error = false;
+      $scope.error        = false;
       $scope.save_element = false;
       $scope.edit_element = true;
-      $scope.modal_title = "Edit token";
+      $scope.modal_title  = "Edit token";
       $('#modal-token').modal('show');
     };
-
-    $scope.updateToken = function() {}
-    $scope.saveToken = function() {}
 
   });
 })();
